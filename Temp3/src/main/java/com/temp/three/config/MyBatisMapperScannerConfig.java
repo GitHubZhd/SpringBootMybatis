aa26cbef6c1d51defefe199437d5484220bb655c
zhanghaidong <zhanghaidong@daihoubang.com>
2017-10-17 18:29:02 +0800

    提交代码

--- Temp3/src/main/java/com/temp/three/config/MyBatisMapperScannerConfig.java
+++ Temp3/src/main/java/com/temp/three/config/MyBatisMapperScannerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 扫描mybatis的接口
+ * 扫描mybatis的接口,其中如下代码为spring自扫描所有dao包并把其下的所有mybatis接口文件装配入容器。
  */
 @Configuration
 // 因为这个对象的扫描，需要在MyBatisConfig的后面注入，所以加上下面的注解
